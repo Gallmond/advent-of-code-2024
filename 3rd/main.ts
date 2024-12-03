afe7c428664bb2e31bbb4cef1d70fd4190dbeed2
Gavin Allmond <gavin.allmond@bynder.com>
2024-12-03 10:53:34 +0000

    reafactor

--- 3rd/main.ts
+++ 3rd/main.ts
@@ -11,20 +11,16 @@ const getMul = (startCursor: number, text: string): MulSolution | undefined => {
   const subStr = text.substring(startCursor, startCursor + max)
 
   // first four characters must be 'mul('
-  if (subStr.indexOf("mul(") !== 0) {
-    return undefined
-  }
+  if (subStr.indexOf("mul(") !== 0) return undefined
 
   // now crawl onwards until we hit a ')'
   const allowedCharacters = "0123456789,)".split("") // fuck regex
-  let foundComma = false
-  let searchOffset = 4 // skip over 'mul('
+
+  let searchOffset = 4, foundComma = false
   main: do {
     const char = subStr.charAt(searchOffset)
 
-    if (!allowedCharacters.includes(char)) {
-      return undefined
-    }
+    if (!allowedCharacters.includes(char)) return undefined
 
     if (char === ",") foundComma = true
 
@@ -35,12 +31,12 @@ const getMul = (startCursor: number, text: string): MulSolution | undefined => {
 
     searchOffset++
 
-    if (searchOffset > max) {
-      break main
-    }
+    if (searchOffset > max) break main
   } while (true)
 
-  return processMulString(text.substring(startCursor, startCursor + searchOffset))
+  return text.substring(startCursor, startCursor + searchOffset)
+    .substring(4).slice(0, -1).split(",").map((s) => parseInt(s))
+    .reduce((acc, current) => acc * current, 1)
 }
 
 const getSwitch = (cursor: number, text: string): SwitchValue | undefined => {
@@ -53,63 +49,34 @@ const getSwitch = (cursor: number, text: string): SwitchValue | undefined => {
   return undefined
 }
 
-const collectMulsAndSwitches = (
-  text: string,
-): { muls: Map<number, MulSolution>; switches: Map<number, SwitchValue> } => {
-  const allMuls = new Map<number, MulSolution>()
-  const allSwitches = new Map<number, SwitchValue>()
+const collectMulsAndSwitches = (text: string) => {
+  const muls = new Map<number, MulSolution>()
+  const switches = new Map<number, SwitchValue>()
 
   for (let cursor = 0, l = text.length; cursor < l; cursor++) {
     const thisChar = text.charAt(cursor)
 
     if (thisChar === "m") {
       const mul = getMul(cursor, text)
-      if (mul !== undefined) allMuls.set(cursor, mul)
-      continue
-    }
-
-    if (thisChar === "d") {
+      if (mul !== undefined) muls.set(cursor, mul)
+    } else if (thisChar === "d") {
       const swi = getSwitch(cursor, text)
-      if (swi !== undefined) allSwitches.set(cursor, swi)
-      continue
+      if (swi !== undefined) switches.set(cursor, swi)
     }
   }
 
-  return {
-    muls: allMuls,
-    switches: allSwitches,
-  }
-}
-
-const processMulString = (mulString: string): number => {
-  const withoutHead = mulString.substring(4) // 'mul('
-  const withoutTail = withoutHead.substring(0, withoutHead.length - 1) // ')'
-  const numbers = withoutTail.split(",").map((n) => parseInt(n))
-
-  if (numbers.length !== 2) {
-    console.error({ mulString })
-    throw new Error("Invalid mulString")
-  }
-
-  return numbers[0] * numbers[1]
+  return { muls, switches }
 }
 
 const getMulSum = (text: string): number => {
   let active = true, solutionAcc = 0
   const { muls, switches } = collectMulsAndSwitches(text)
-  for (let i = 0, l = text.length; i < l; i++) {
-    const swi = switches.get(i)
-    if (swi !== undefined) active = swi
-
-    const mulSolution = muls.get(i)
-    if (active && mulSolution !== undefined) {
-      solutionAcc += mulSolution
-    }
+  for (let cursor = 0, l = text.length; cursor < l; cursor++) {
+    active = switches.get(cursor) ?? active
+    if (active) solutionAcc += muls.get(cursor) ?? 0
   }
 
   return solutionAcc
 }
 
-const solution = getMulSum(text_case)
-
-console.log("the total of all the mul(x,y) processing with switch is", { solution })
+console.log("the total of all the mul(x,y) processing with switch is", getMulSum(text_case))
